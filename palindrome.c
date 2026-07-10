a864bc95f045b6ff3eeae1bf26aa6a2c2fa03d18
Keshavkarthikeyan07 <keshavkarthikeyan07@gmail.com>
2026-07-10 18:53:05 +0530

    Add files via upload

---
+++ palindrome.c
@@ -0,0 +1,17 @@
+bool isPalindrome(int x)
+{
+    if (x < 0)
+        return false;
+
+    int temp = x;
+    long long reverse = 0;
+
+    while (temp != 0)
+    {
+        int digit = temp % 10;
+        reverse = reverse * 10 + digit;
+        temp = temp / 10;
+    }
+
+    return reverse == x;
+}
